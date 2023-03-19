72b7869af6c33e02923b06bbcd9d0aa8a4bb642d
TONYGUDIMA <91024647+TONYGUDIMA@users.noreply.github.com>
2023-03-19 18:46:19 +0000

    Deploying to gh-pages from @ TONYGUDIMA/goit-js-hw-09@2d2cda748a69911fdb39406564c6c05192885cef 🚀

---
+++ 01-color-switcher.39805438.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");const d=function(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;r.style.backgroundColor=t};t.addEventListener("click",(()=>{t.disabled=!0,timerId=setInterval(d,1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.39805438.js.map
