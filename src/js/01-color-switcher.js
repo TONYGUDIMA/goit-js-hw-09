2d2cda748a69911fdb39406564c6c05192885cef
TONYGUDIMA <gudima.anton@yahoo.com>
2023-03-19 20:45:54 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+const body = document.querySelector('body')
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+const changesColor = function() {
+  const randomColor = getRandomHexColor()
+  body.style.backgroundColor = randomColor
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true
+  timerId = setInterval(changesColor, 1000)
+})
+
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false
+  clearInterval(timerId)
+})
 
